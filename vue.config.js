9672c4196e601b54feeb8bb78e92a4df65daaa44
zhangyunzhong <1070461889@qq.com>
2021-04-12 10:04:02 +0800

    feat:封装vuex

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
  * @Author: Yi Yunwan
  * @Date: 2020-09-04 12:06:22
  * @LastEditors: Zhang Yunzhong
- * @LastEditTime: 2021-02-22 11:03:42
+ * @LastEditTime: 2021-03-18 11:49:38
  */
 module.exports = {
   publicPath: './',
@@ -17,6 +17,14 @@ module.exports = {
         maxAssetSize: 30000000
       }
     }
+  },
+  // 20kb以下图片转为base64
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, { limit: 20000 }))
   },
   css: {
     extract: false,
